bd8fafcdda2f74eda7560f8ba7eb9f87cd400e41
dengb <dengbin19910916@163.com>
2017-09-30 07:59:13 +0800

    Initial commit

---
+++ src/main/java/com/topscore/omnichannel/promotion/PromotionApplication.java
@@ -0,0 +1,12 @@
+package com.topscore.omnichannel.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionApplication.class, args);
+	}
+}
